7aa808be042ab99db8d0ccf4253246a9d2e4ca02
duncan.gichimu <duncangichimu@gmail.com>
2025-06-25 11:30:25 -0700

    Refactor GitHub Actions OIDC provider configuration in Terraform module to use data source instead of resource. This change simplifies the setup by removing unnecessary attributes, enhancing maintainability and clarity in the deployment process.

--- Infrastructure/terraform/modules/github-actions-codedeploy.tf
+++ Infrastructure/terraform/modules/github-actions-codedeploy.tf
@@ -1,21 +1,6 @@
 # GitHub Actions OIDC Provider for CodeDeploy deployments
-resource "aws_iam_openid_connect_provider" "github_actions_codedeploy" {
+data "aws_iam_openid_connect_provider" "github_actions_codedeploy" {
   url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1",
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
-  ]
-
-  tags = {
-    Name        = "${var.app_name}-github-actions-oidc-codedeploy"
-    Environment = var.environment
-    Purpose     = "GitHub Actions CodeDeploy Deployments"
-  }
 }
 
 # IAM Policy for GitHub Actions CodeDeploy deployments
