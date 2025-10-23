11231aa386b2c6313bf6fe7ff7b5609f45da7f95
duncan.gichimu <duncangichimu@gmail.com>
2025-10-22 19:53:26 -0700

    Refactor Terraform module configuration for improved readability
    - Adjusted formatting of variable assignments in the auth module configuration within main.tf for better clarity and consistency.

--- infrastructure/terraform/dev/main.tf
+++ infrastructure/terraform/dev/main.tf
@@ -1,9 +1,9 @@
 module "auth" {
   source = "../modules-single-az/auth"
 
-  region                 = var.region
-  project_name           = var.project_name
-  env                    = var.env
+  region       = var.region
+  project_name = var.project_name
+  env          = var.env
 
   idps                     = var.idps
   auth_callback            = var.auth_callback
